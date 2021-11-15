f11bf1fba47c515d689866432c8d0d2eb0a3896b
45NGC <79377468+45NGC@users.noreply.github.com>
2021-11-15 10:56:36 +0100

    Añadido menuPrincipal a Main
    Menu con un switch que nos lleva a metodos vacios en los cuales introduciremos el codifo de nuestros respectivos proyectos

--- src/clasesPrincipales/Main.java
+++ src/clasesPrincipales/Main.java
@@ -1,7 +1,93 @@
 package clasesPrincipales;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[]args){
+    public static void main(String[]args) throws IOException{
+        Teclado t = new Teclado();
+        int opcionMenuPrincipal;
+        boolean fin=false;
+
+        do{
+            opcionMenuPrincipal = menuPrincipal();
+            switch (opcionMenuPrincipal){
+                case 1 :
+                    clientes();
+                    break;
+                case 2:
+                    ventaDeEntradas();
+                    break;
+                case 3:
+                    eventos();
+                    break;
+                case 4:
+                    tienda();
+                    break;
+                case 5:
+                    peceras();
+                    break;
+                case 6:
+                    peces();
+                    break;
+                case 7:
+                    empleados();
+                    break;
+                case 8:
+                    fin = true;
+                    break;
+            }
+        }while(!fin);
+
+    }
+
+    static int menuPrincipal() throws IOException{
+        Teclado t = new Teclado();
+        int opcion;
+        System.out.println();
+        System.out.println("Seleccione una opcion :");
+        System.out.println();
+        System.out.println("1 - Clientes");
+        System.out.println("2 - Venta de entradas");
+        System.out.println("3 - Eventos");
+        System.out.println("4 - Tienda");
+        System.out.println("5 - Peceras");
+        System.out.println("6 - Peces");
+        System.out.println("7 - Empleados");
+        System.out.println("8 - Fin");
+        System.out.println();
+        do{
+            System.out.println("Seleccione una opcion (1-8) : ");
+            opcion = t.leerInt();
+        }while(opcion<1 || opcion>8);
+
+        return opcion;
+    }
+
+    static void clientes(){
+
+    }
+
+    static void ventaDeEntradas(){
+
+    }
+
+    static void eventos(){
+
+    }
+
+    static void tienda(){
+
+    }
+
+    static void peceras(){
+
+    }
+
+    static void peces(){
+
+    }
+
+    static void empleados(){
 
     }
 }
